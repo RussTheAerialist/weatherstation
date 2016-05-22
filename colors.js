b6c0c2d46e0766fa67e2d69bb442876be1bddbd6
Russell Hay <me@russellhay.com>
2016-05-21 17:35:40 -0700

    Improve color gradient and add locations hash

--- colors.js
+++ colors.js
@@ -1,16 +1,28 @@
 var one = require('onecolor')
-var startColor = one('blue')
-var endColor = one('red')
 
-var colors = []
-for(var i=0; i<30;i++) {
-  var percent = i/30.0
-  var red = (startColor.red() + percent * (endColor.red() - startColor.red()))
-  var green = (startColor.green() + percent * (endColor.green() - startColor.green()))
-  var blue = (startColor.blue() + percent * (endColor.blue() - startColor.blue()))
-  var nextColor = one(['RGB', red, green, blue, 1.0]).hex()
-  colors.push([i, nextColor])
+function getColor(c1, c2, percent) {
+  var red = (c1.red() + percent * (c2.red() - c1.red()))
+  var green = (c1.green() + percent * (c2.green() - c1.green()))
+  var blue = (c1.blue() + percent * (c2.blue() - c1.blue()))
+  return one(['RGB', red, green, blue, 1.0]).hex()
 }
 
+function threecolor(c1, c2, c3, steps) {
+  var colors = []
+  var i
+
+  for(i=0.0; i<steps; i++) {
+    var color = getColor(
+      i < steps/2.0 ? c1 : c2,
+      i < steps/2.0 ? c2 : c3,
+      i < steps/2.0 ? i/(steps/2.0) : (i-steps/2.0)/(steps/2.0)
+    )
+    colors.push([i, color])
+  }
+
+  return colors
+}
+
+var colors = threecolor(one('blue'), one('green'), one('red'), 30)
 module.exports = colors
 console.log(colors)
